9e07781f8ce7388b9acde92791db375c501dc003
mndayiragi-hub <m.ndayiragi@alustudent.com>
2026-06-04 13:28:29 +0200

    Add secure_data function

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-initialize_system(){
-     i="active_logs"
-     a="archived_logs"
-     r="reports"
-
-  if [ -d "$i" ];
-     then
-       echo "file $i does exist"
-  else
-    echo "file doesn't exist; creating active_logs directory"
-    echo $(mkdir $i)
-
- fi
-
-  if [ -d "$a" ];
-     then
-       echo "file $a does exist"
-  else
-   echo "file doesn't exist; creating archived_logs directory"
-    echo $(mkdir $a)
-
- fi
-
-  if [ -d "$r" ];
-     then
-       echo "file $r does exist"
-  else
-    echo "file doesn't creating reports directory"
-    echo $(mkdir $r)
-
- fi
+secure_data() {
+    chmod 700 active_logs
 }
