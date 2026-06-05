fcf624cd82ced2cef8a3ce62046b7701931a86ab
Daniella <m.nishimwe@alustudent.com>
2026-06-05 19:41:21 +0200

    I have added a command that prints and displays the creation of the three directories

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -9,26 +9,38 @@ initialize_system(){
      then
        echo "file $i does exist"
   else
-    echo "file doesn't exist; creating active_logs directory"
+    echo "file $i doesn't exist; creating active_logs directory"
     echo $(mkdir $i)
+    echo $(sleep 2)
+    echo "the file $i has been created"
 
  fi
+
+ echo $(sleep 2)
 
   if [ -d "$a" ];
      then
        echo "file $a does exist"
   else
-   echo "file doesn't exist; creating archived_logs directory"
+   echo "file $a doesn't exist; creating archived_logs directory"
     echo $(mkdir $a)
+      echo $(sleep 2)
+    echo "the file $a has been created"
 
  fi
+ echo $(sleep 2)
 
   if [ -d "$r" ];
      then
        echo "file $r does exist"
   else
-    echo "file doesn't creating reports directory"
+    echo "file $r doesn't creating reports directory"
     echo $(mkdir $r)
+      echo $(sleep 2)
+    echo "the file $r has been created"
+
 
  fi
+ echo $(sleep 2)
+ echo "$(ls -ld */)"
 }
